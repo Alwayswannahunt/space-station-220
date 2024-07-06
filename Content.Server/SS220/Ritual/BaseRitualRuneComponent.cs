af3d5ef746e8e02472d0a73542bc81a625d4cbff
Anri <elyasin.ane@mail.ru>
2024-07-06 08:54:10 +0300

    FirstStep

---
+++ Content.Server/SS220/Ritual/BaseRitualRuneComponent.cs
@@ -0,0 +1,8 @@
+namespace Content.Server.SS220.Ritual;
+
+[RegisterComponent]
+public sealed partial class BaseRitualRuneComponent : Component
+{
+
+
+}
---
+++ Content.Server/SS220/Ritual/BaseRitualRuneSystem.cs
@@ -0,0 +1,11 @@
+namespace Content.Server.SS220.Ritual;
+public sealed partial class BaseRitualRuneSystem : EntitySystem
+{
+
+
+
+    // private bool IsAvaible(){
+    //     if (Anchored == false)
+    //         idi nahui
+    // }
+}
